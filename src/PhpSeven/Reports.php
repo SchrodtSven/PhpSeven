0cd122d7b53ce97fb2d26c1d42997a00417bf577
Sven Schrodt <sven@schrodt.club>
2025-10-23 13:50:24 +0200

    Finishing example for Covariance and Contravariance

--- src/PhpSeven/Reports.php
+++ src/PhpSeven/Reports.php
@@ -15,6 +15,10 @@ declare(strict_types=1);
 
 namespace  SchrodtSven\PhpSeven;
 
+# Covariance
+
+printf("Covariance%s",PHP_EOL);
+
 abstract class  Report
 {
     protected string $name;
@@ -31,7 +35,7 @@ class DashReport extends  Report
 {
     public function render()
     {
-        echo $this->name . " implemented with Plotly Dash";
+        printf("%s implemented with Plotly Dash%s", $this->name ,PHP_EOL);
     }
 }
 
@@ -39,7 +43,7 @@ class BIReport extends  Report
 {
     public function render()
     {
-        echo $this->name . " implemented with M$ Power BI";
+        printf("%s implemented with M$ Power BI%s", $this->name ,PHP_EOL);
     }
 }
 
@@ -47,3 +51,66 @@ interface ReportFactory
 {
     public function build(string $name): Report;
 }
+
+class BIFactory implements ReportFactory
+{
+    public function build(string $name): BIReport
+    {
+        return new BIReport($name);
+    }
+}
+
+class DashFactory implements ReportFactory
+{
+    public function build(string $name): DashReport
+    {
+        return new DashReport($name);
+    }
+}
+
+
+$bookOne = (new BIFactory)->build("A. new Report");
+
+$bookTwo = (new DashFactory)->build("B. new Report");
+
+$bookOne->render();
+
+$bookTwo->render();
+
+
+printf("Contravariance%s",PHP_EOL);
+
+class Account {}
+
+class BankAccount extends Account {}
+
+abstract class User
+{
+    protected string $name;
+
+    public function __construct(string $name)
+    {
+        $this->name = $name;
+    }
+
+    public function consume(BankAccount $account)
+    {
+        printf("%s consumes %s%s", $this->name, $account::class ,PHP_EOL);
+        
+    }
+}
+
+class FooUser extends User
+{
+    public function consume(Account $account) {
+        printf("%s consumes %s%s", $this->name,$account::class ,PHP_EOL);
+        
+    }
+}
+
+
+$usrOne = new FooUser("Gwen");
+$usrOne->consume(new Account());
+
+$usrOne = new FooUser("Mary Jane");
+$usrOne->consume(new BankAccount());
